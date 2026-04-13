f6fd7831a4fcf554aa1481057e07a0a75e9e5c5b
plaiboycaty <mquan8912@gmail.com>
2026-04-13 08:59:22 +0700

    abc

--- App.tsx
+++ App.tsx
@@ -17,7 +17,6 @@ import type {
   MainTabParamList,
   AppStackParamList,
 } from "@/types/navigation.types";
-// Re-export để các file cũ import từ App.tsx vẫn hoạt động
 export type { AuthStackParamList, MainTabParamList, AppStackParamList };
 
 // ─── Auth Screens ─────────────────────────────────────────────────────────────
